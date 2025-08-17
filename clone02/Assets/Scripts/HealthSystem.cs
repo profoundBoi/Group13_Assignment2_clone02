4b8fee19335cf4be31343ef2f23bd0c3cf99cd6c
profoundBoi <connor170103@gmail.com>
2025-08-17 22:46:58 +0200

    Respawn and enemy kill player

--- clone02/Assets/Scripts/HealthSystem.cs
+++ clone02/Assets/Scripts/HealthSystem.cs
@@ -1,18 +1,51 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Respawn Settings")]
+    [SerializeField] private Transform startingRespawnPoint;
+    private Transform lastRespawnPoint;
+
+    [Header("Death Effects")]
+    [SerializeField] private float respawnDelay = 0.5f;
+
+    [Header("References")]
+    [SerializeField] private CharacterController characterController; 
+
+    private void Start()
     {
-        
+        if (startingRespawnPoint != null)
+            lastRespawnPoint = startingRespawnPoint;
+
+        if (characterController == null)
+            characterController = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Enemy"))
+        {
+            Die();
+        }
+
+        if (other.CompareTag("Respawn"))
+        {
+            lastRespawnPoint = other.transform;
+        }
+    }
+
+    private void Die()
+    {
+        characterController.enabled = false;
+
+        if (lastRespawnPoint != null)
+        {
+            transform.position = lastRespawnPoint.position + Vector3.up * 0.1f; 
+        }
+
+        characterController.enabled = true;
+
     }
 }
